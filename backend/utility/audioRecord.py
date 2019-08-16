319dd638600db2407e904189c2c82ba15c795fd2
Damien <min.dai.chn@outlook.com>
2019-08-16 23:31:43 +0800

    增加测试摄像头功能

---
+++ backend/utility/audioRecord.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import pyaudio
+import wave
+import os
+import sys
+
+__all__ = [
+        'audioRecord',
+        ]
+
+CHUNK = 512
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 24000
+RECORD_SECONDS = 5
+
+
+# 录音
+def audioRecord(wavFile, seconds):
+    p = pyaudio.PyAudio()
+    stream = p.open(format = FORMAT,
+                    channels = CHANNELS,
+                    rate = RATE,
+                    input = True,
+                    frames_per_buffer = CHUNK)
+    frames = []
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(wavFile, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+# 播放
+def audioPlay(wavFile):
+    wf = wave.open(wavFile, 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output = True)
+    data = wf.readframes(CHUNK)
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf.close()
+
+
+if __name__ == '__main__':
+    wavFile = 'test.wav'
+    if os.path.isfile(wavFile):
+        try:
+            os.remove(wavFile)
+        except:
+            pass
+    audioRecord(wavFile, 5)
+    audioPlay(wavFile)
+    if os.path.isfile(wavFile):
+        try:
+            os.remove(wavFile)
+        except:
+            pass
+
+
---
+++ backend/utility/camera.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+
+import os
+import base64
+import time
+import platform
+
+if platform.system().lower() == 'windows':
+    import cv2
+elif platform.system().lower() == 'linux':
+    from picamera import PiCamera
+else:
+    pass
+
+
+# 拍照
+def capturePicture(picFile):
+    if platform.system().lower() == 'windows':
+        cap = None
+        ret = False
+        try:
+            cap = cv2.VideoCapture(0)
+            if cap.isOpened():
+                cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+                cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+                ret, frame = cap.read()
+                if ret:
+                    cv2.imwrite(picFile, frame)
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()
+            return ret
+    elif platform.system().lower() == 'linux':
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            camera.start_preview()
+            time.sleep(2)
+            camera.capture(picFile)
+            return True
+        return False
+    else:
+        return False
+
+
+# 将图片转换为 Base64 格式
+def pictureToBase64(picFile):
+    x = ''
+    with open(picFile, 'rb') as f:
+        x = base64.b64encode(f.read())
+    return x
+
+
+if __name__ == '__main__':
+    picFile = 'test.jpg'
+    if os.path.isfile(picFile):
+        try:
+            os.remove(picFile)
+        except:
+            pass
+    capturePicture(picFile)
