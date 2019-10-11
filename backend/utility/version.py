6031288c53e9def6c7d7617eb9afcff07434de6a
Damien <min.dai.chn@outlook.com>
2019-10-11 17:49:51 +0800

    增加版本信息

---
+++ backend/utility/version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+
+VERSION = '0.99'
+
+# 获取版本信息
+def getVersion():
+    return VERSION
+
+
+################################################################################
+# 测试程序
+if __name__ == '__main__':
+    ver = getVersion()
+    print(ver)
